a6e1417b9b8d91795825ee06d0a4e1e94be05217
SabineZilde <sabine.zilde@protonmail.ch>
2021-03-25 21:26:33 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function changeStyle() {
+    const h2Element = document.getElementById('important-h2-element');
+    if (h2Element.style.color === 'red') {
+        h2Element.style.color = 'black ';
+        h2Element.style.fontSize = '24px';
+        h2Element.innerText = 'Some h2 element!'
+    } else {
+        h2Element.style.color = 'red';
+        h2Element.style.fontSize = '56px';
+        h2Element.innerText = 'Hello world!'
+    }
+}
